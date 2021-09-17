539beb511ed9c43517859d781cadf272f54dca30
briandgomez <briandgomez98@gmail.com>
2021-09-16 23:07:56 -0400

    start

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 //Reference to body
 var body = document.body
-var headerEl = document.querySelector("#view-high-score");
-var timerEl = document.querySelector("#time");
+var highScoreEl = document.getElementById("view-high-score");
+var timerEl = document.getElementById("time");
 
-//View high scores (Save to localStorage)
-function viewHighScore() {
+//Display title, message and start button
+var quizTitle = document.createElement('h1');
+quizTitle.textContent = 'Coding Quiz Challenge';
+quizTitle.setAttribute('style', 'text-align:center');
+quizTitle.addClassName = 
+document.body.appendChild(quizTitle);
 
-}
+var quizMessage = document.createElement('p');
+quizMessage.textContent = 'Answer the following questions related to javascript. Everytime you select a incorret answer the timer will decrease by 10 seconds!';
+quizMessage.setAttribute('style', 'text-align:center');
+document.body.appendChild(quizMessage);
 
-//Countdown until 0 and end quiz
-function timer() {
-    var timeLeft = 75;
+var startButton = document.createElement('button');
+startButton.textContent = 'Start';
+startButton.style.display = 'flex';
+startButton.style.margin = 'auto';
+document.body.appendChild(startButton);
 
-    var timeInterval = setInterval(function() {
-        if (timeLeft === 0) {
-            clearInterval(timeLeft);
-            //Display message here
+//Add event listenre for button to move onto next page
+startButton.addEventListener('click', function () {
+    firstQuestion();
+});
+
+//Quiz questions
+var parent = document.createElement('div');
+
+function firstQuestion() {
+    quizTitle.style.display ='none';
+    quizMessage.style.display ='none';
+    startButton.style.display ='none';
+
+    //Object that contains questions
+    var questions = [
+        {
+            question: 'What does HTML stand for?',
+            choices: ['Hypertext Markup Language','Hypertool and Markup Language', 'c.	Hypertext Match Language','d.	None of the above'],
+            answer: choices[0],
         }
-        else {
-            timeLeft--;
-            timerEl.textContent = timeLeft + 'seconds left';
-        }
-    }, 1000)
-}
-
-var startQuiz = function() {
-    var startButton = document.querySelector("#start-btn");
-    startButton.onclick = timer();
+    ]
 }
