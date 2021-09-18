309495d82412f4d1f8f0c1f3e19974c76a06d31b
briandgomez <briandgomez98@gmail.com>
2021-09-18 13:20:47 -0400

    select correct answer

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //Reference to body
 var body = document.body
+
+//Variables retrieved from HTML
 var highScoreEl = document.getElementById("view-high-score");
 var timerEl = document.getElementById("time");
 var containerEl = document.getElementsByClassName("container");
@@ -23,23 +25,60 @@ startButton.style.display = 'flex';
 startButton.style.margin = 'auto';
 document.body.appendChild(startButton);
 
-//Add event listenre for button to move onto next page
-startButton.addEventListener('click', function () {
-    startTimer();
-    startQuiz();
-});
+//Global variables used by the functions
+var currentTime = 50;
+var currentQuestion = 0;
+var currentScore = 0;
 
-var currentTime = 5;
-//Start timer
+//Questions array
+var questionObject = [
+    {
+        q: '1. What does ‘HTML’ stand for?',
+        a: [
+            'Hypertext Markup Language', 'Hypertool Markup Language', 'Hypertext Match Language'
+        ],
+        correct: 'Hypertext Markup Language'
+    },
+    {
+        q: '2. Arrays are created using which of the following?',
+        a: [
+            'parentheses', 'curly brackets', 'brackets'
+        ],
+        correct: 'brackets'
+    },
+    {
+        q: '3. How do you initialize flexbos when using CSS?',
+        a: [
+            'display: flex', 'display flex', 'display: block'
+        ],
+        correct: 'display: flex'
+    },
+    {
+        q: '4. Commonly used data types do NOT include:',
+        a: [
+            'strings', 'booleans', 'alerts'
+        ],
+        correct: 'alerts'
+    },
+    {
+        q: '5. How do you create a numbered list?',
+        a: [
+            '<ul>', '<ol>', '<nl>'
+        ],
+        correct: '<ol>'
+    },
+
+]
+
+//Starts timer
 function startTimer() {
 
     var timeInterval = setInterval(function () {
-        //
+        //Stops timer when time runs out
         if (currentTime === 0) {
             clearInterval(timeInterval);
-
-            //Function to submit initials and score 
         }
+        //Countdown for timer
         else {
             currentTime--;
             timerEl.textContent = 'Time:' + currentTime;
@@ -48,67 +87,77 @@ function startTimer() {
 
 }
 
-
+//Clears 'intro' items(title, message, button) and calls questions
 function startQuiz() {
     quizTitle.style.display = 'none';
     quizMessage.style.display = 'none';
     startButton.style.display = 'none';
 
-    //Function that displays and iterates through questions
     questions();
 }
 
-
-var currentQuestion = 0;
-var questionObject = [
-    {
-        q: '1.	What does ‘HTML’ stand for?',
-        a: [
-            'a', 'b', 'c'
-        ],
-        correct: 'c'
-    },
-    {
-        q: '2. What is?',
-        a: [
-            'd', 'e', 'f'
-        ],
-        correct: 'c'
-    },
-
-]
-
-
+//Displays questions
 function questions() {
+    //Assigns question to div element
+    console.log('Current question', currentQuestion);
+    if (currentQuestion >= questionObject.length || currentTime === 0) {
+        return checkHighScore();
+    }
+
     questionEl.textContent = questionObject[currentQuestion].q
 
-    //Clears answers
+    //Clears answers div element
     answerEl.textContent = '';
 
+    //Iterates through the length of the answers
     for (var i = 0; i < questionObject[currentQuestion].a.length; i++) {
         var answerbutton = document.createElement('button');
         answerbutton.textContent = questionObject[currentQuestion].a[i];
-        answerbutton.onclick = checkAnswer;
+        answerbutton.addEventListener('click', function(){
+            checkAnswer(this.textContent);
+        });
         answerEl.appendChild(answerbutton);
     }
 }
 
 
-function checkAnswer() {
+
+function checkAnswer(answer) {
+    var correctSelected = questionObject[currentQuestion].correct;
+    console.log('Answer', answer);
+
+    if (answer === correctSelected) {
+        //console.log(correctSelected)
+        currentScore++;
+        //console.log(currentScore)
+    } else {
+        console.log('incorrect')
+        currentTime -= 10;
+    }
+
     currentQuestion++;
+    console.log('Checkanswers icnrement', currentQuestion);
     questions();
 }
 
-var currentScore = 5;
-
 function checkHighScore() {
+    //Assigns a variable with the value of the highscore
     var currentHighScore = localStorage.getItem('quizHighScore');
 
+    //Checks if a high score currently exists OR if the current score is is greater than the high score
     if (!currentHighScore || currentHighScore < currentScore) {
         var userInitials = prompt('Enter your initals')
         localStorage.setItem('quizHighScore', currentScore);
         localStorage.setItem('quizHighInitials', userInitials)
     } else {
-        alert('Current score was not high enough');
+        alert('Current score was not high enough' + ' Your score: ' + currentScore);
     }
+
+    alert('High Score: ' + currentHighScore);
 }
+
+//Add event listener for button to move onto next page
+startButton.addEventListener('click', function () {
+    startTimer();
+    startQuiz();
+});
